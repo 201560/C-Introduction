e5dbdbfdda2cde9ea397c7a9030b4d539f0a38ee
201560 <80399894+201560@users.noreply.github.com>
2021-03-31 09:01:11 -0400

    Created a number guessing game.

---
+++ Number_Game.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+        int number;
+        int guess;
+        int play;
+        int new_game;
+        new_game = 1;
+        while (new_game == 1) {
+                srand(time(0));
+                number = rand() % 10 + 1;
+                play = 1;
+                while (play == 1) {
+                        cout << "Guess my favorite number.  Hint: it is from 1-10." << endl;
+                        cin >> guess;
+                        if (number == guess) {
+                                cout << "Correct!" << endl;
+                                play = 0;
+                        }
+                        if (number != guess) {
+                                cout << "Incorrect" << endl << "Do you want to guess again? (Enter 1 for yes and 0 for no)" << endl;
+                                cin >> play;
+                        }
+                }
+                cout << "My favorite number is " << number << " and you guessed " << guess << "." << endl << "Do you want to play again with$
+                cin >> new_game;
+        }
+        return 0;
+}
