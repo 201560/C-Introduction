46a78bb080210bf0846cbcece0d8801ba1c9097f
201560 <80399894+201560@users.noreply.github.com>
2021-03-31 12:25:48 -0400

    Create a calculator that converts Fahrenheit to Celsius.

---
+++ Fahrenheit_to_Celsius.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+        float fahrenheit;
+        float celsius;
+        fahrenheit = 0;
+        celsius = 0;
+        cout << "Enter the temperature in fahrenheit." << endl;
+        cin >> fahrenheit;;
+        celsius = (fahrenheit - 32)*5/9;
+        cout << "The temperature is " << celsius << " degrees celsius." << endl;
+        return 0;
+}
