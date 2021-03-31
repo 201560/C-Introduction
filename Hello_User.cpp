be2cd784bda8a5724f1a2a1468fffefeaf7557c0
201560 <80399894+201560@users.noreply.github.com>
2021-03-31 08:06:36 -0400

    Created a program that returns "Hello 'user's name' "

---
+++ Hello_User.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+        std :: string name;
+        std :: cout << "What is your first name?\n";
+        std :: cin >> name;
+        std :: cout << "Hello " << name << std :: endl;
+        return 0;
+}
