8ac17ece23dc441b36153cfcd37c516c7c4f3d0e
201560 <80399894+201560@users.noreply.github.com>
2021-04-01 09:10:11 -0400

    Create a file that writes to a file.

---
+++ Text_File.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+	string input;
+	cout << "What do you want to put in the text file?" << endl << "Type EXIT on a new line to close" << endl;
+	ofstream file;
+        file.open("Text.txt", ios_base :: app);
+	while (input!="EXIT") {
+		getline (cin, input);
+		if (input != "EXIT") {
+			file << input << " ";
+		}
+	}
+	file.close();
+}
